a8a64314b13f72e3f27f84b3f1dc8024a59105c5
Luis Ulloa <timken@gmail.com>
2018-07-31 12:40:50 +0100

    removing unused imports

--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-import os
-import sys
-import subprocess
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
-
-# # TODO is there a better way ? dependencies seem to always require the version
-# # Calling only at the egg_info step gives us the wanted depth first behavior
-# if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
-#     pip3_executable = os.path.join(os.path.abspath(os.path.join(os.path.dirname(sys.executable))), 'pip3')
-#     if not os.path.isfile(pip3_executable):
-#         pip3_executable = 'pip3'
-#     subprocess.check_call([pip3_executable, 'install', '--no-warn-conflicts', '--upgrade', '-r', 'requirements.txt'])
 
 setup(
     name=NAME,
