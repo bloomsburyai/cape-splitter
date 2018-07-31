d54a0dde3d0eca3640e1502b4aed17b1abccb8e0
Luis Ulloa <timken@gmail.com>
2018-07-31 12:03:19 +0100

    Improving setup

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import subprocess
 from package_settings import NAME, VERSION, PACKAGES, DESCRIPTION
 from setuptools import setup
 
-# TODO is there a better way ? dependencies seem to always require the version
-# Calling only at the egg_info step gives us the wanted depth first behavior
-if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
-    pip3_executable = os.path.join(os.path.abspath(os.path.join(os.path.dirname(sys.executable))), 'pip3')
-    if not os.path.isfile(pip3_executable):
-        pip3_executable = 'pip3'
-    subprocess.check_call([pip3_executable, 'install','--force-reinstall','--upgrade', '-r', 'requirements.txt'])
+# # TODO is there a better way ? dependencies seem to always require the version
+# # Calling only at the egg_info step gives us the wanted depth first behavior
+# if 'egg_info' in sys.argv and os.getenv('CAPE_DEPENDENCIES', 'False').lower() == 'true':
+#     pip3_executable = os.path.join(os.path.abspath(os.path.join(os.path.dirname(sys.executable))), 'pip3')
+#     if not os.path.isfile(pip3_executable):
+#         pip3_executable = 'pip3'
+#     subprocess.check_call([pip3_executable, 'install', '--no-warn-conflicts', '--upgrade', '-r', 'requirements.txt'])
 
 setup(
     name=NAME,
@@ -21,6 +21,9 @@ setup(
     packages=PACKAGES,
     package_dir={'docqa': 'document-qa/docqa'},
     include_package_data=True,
+    install_requires=['Cython==0.27.3',
+                      'dataclasses==0.6',
+                      'pytest==3.6.4'],
     package_data={
         '': ['*.*'],
     },
